cba7b9511328c4374b96e63a8ad42006edb68a70
VadymLityuk <57510267+VadymLityuk@users.noreply.github.com>
2019-11-15 14:10:11 -0500

    1

---
+++ Programation1/Chapitre2/js/exercice4.js
@@ -0,0 +1,15 @@
+
+var nb1 = prompt("Entrez le nombre 1 :");
+var nb2 = prompt("Entrez le nombre 2 :");
+var resultat ;
+
+resultat = nb1/nb2;
+ document.write (resultat);
+
+nb2 != 0
+document.write ("Division par zéro interdite")
+
+
+
+
+
---
+++ Programation1/Chapitre2/js/q5.js
@@ -0,0 +1,11 @@
+
+var nb = prompt("Entrez le temperature :");
+
+
+if (nb < -40 || nb < -10 ) {
+    document.write ("Il fait froide");
+}
+
+if ( nb > -10 ) {
+    document.write ("Enfin une belle journee");
+}
