5db315332ec8f7b706edbb211cbe62b5997fd909
VadymLityuk <57510267+VadymLityuk@users.noreply.github.com>
2019-11-14 19:23:49 -0500

    1

---
+++ Programation1/Chapitre2/js/q1.js
@@ -0,0 +1,19 @@
+// lire deux nombres et afficher le plus grand des deux
+
+var nb1
+var nb2
+
+nb1 = Number(prompt("Entrez le nombre 1 : ")) ;
+nb2 = Number(prompt("Entrez le nombre 2 : ")) ;
+
+if (nb1 > nb2 ) {
+    document.write ("le nombre 1 est le plus grand :" + nb1 ) ;
+}
+
+else if(nb2>nb1) {
+    document.write("le nombre 2 est le plus grand : " + nb2);
+}
+
+else {
+    document.write( "Les nombres sont egaux") ;
+}
