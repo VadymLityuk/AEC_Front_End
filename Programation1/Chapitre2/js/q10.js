1f1a6150fe758e3d84436f57300d092e8bb30080
VadymLityuk <57510267+VadymLityuk@users.noreply.github.com>
2019-11-19 17:32:43 -0500

    1

---
+++ Programation1/Chapitre2/js/q10.js
@@ -0,0 +1,56 @@
+
+var nb = prompt("Entrez le nom un nombre du mois :");
+
+
+if ( nb === "1"){
+    document.write ("Janvier");
+}
+
+if ( nb === "2") {
+    document.write ( "Fevrier");
+}
+if ( nb === "3"){
+    document.write ("Mars") ;
+}
+if ( nb === "4"){
+    document.write ("Avril");
+}
+
+if ( nb === "5") {
+    document.write ( "Mai");
+}
+if ( nb === "6"){
+    document.write ("Juin") ;
+}
+if ( nb === "7"){
+    document.write ("Juillet");
+}
+
+if ( nb === "8") {
+    document.write ( "Août");
+}
+if ( nb === "9"){
+    document.write ("Septembre") ;
+}
+if ( nb === "10"){
+    document.write ("Octobre");
+}
+
+if ( nb === "11") {
+    document.write ( "Novembre");
+}
+if ( nb === "12"){
+    document.write ("Decembre") ;
+}
+
+if ( nb > 12 ) {
+    document.write ("Mois invalide");
+}
+if (nb < 1){
+    document.write ("Mois invalide ");
+}
+
+
+
+
+
---
+++ Programation1/Chapitre2/js/q7.js
@@ -0,0 +1,23 @@
+
+var nb = prompt("Enter the number") ;
+
+
+if ( nb >= "80" && nb <= 90  ) {
+    document.write ("C");
+}
+
+if ( nb >= 90 ){
+    document.write ( "E ");
+}
+
+if ( nb >= 70 && nb <= 80  ) {
+    document.write ("D");
+}
+
+if ( nb >= 60 && nb <70 ){
+    document.write ("B");
+}
+if ( nb < 60){
+    document.write ("A");
+}
+
