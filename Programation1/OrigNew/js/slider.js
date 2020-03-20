e36c658990c8d94b10d231fed45c3cbdb3cd7526
VadymLityuk <57510267+VadymLityuk@users.noreply.github.com>
2020-03-20 00:27:48 -0400

    4

--- Programation1/OrigNew/js/slider.js
+++ Programation1/OrigNew/js/slider.js
@@ -1,38 +1,38 @@
 function imageComparison(selector) {
+
     let comparison = $(selector)
         .addClass('image-comparison')
-        .prepend('<div class="image-comparison_before></div>')
-        .append('<button class="image-comparison_slider"></button>');
+        .prepend('<div class="image-comparison__before"></div>')
+        .append('<button class="image-comparison__slider"></button>');
 
     let images = comparison
         .find('img')
-        .addClass('image-comparison_image')
+        .addClass('image-comparison__image')
         .css('max-width', comparison.width());
 
     let before = comparison
-        .find('image-comparison_before')
+        .find('.image-comparison__before')
         .append(images.eq(0));
 
     comparison
-        .find('.image-comparison_slider')
-        .on('dragstart',() => false)
-        .on('mousedown',function (e) {
-            let  slider = $(this);
+        .find('.image-comparison__slider')
+        .on('dragstart', () => false) // fals. drug&drop
+        .on('mousedown', function(e) {
+            let slider = $(this);
+            let doc = $(document).on('mousemove', (e) => {
+                let offset = e.pageX - comparison.position().left;
+                let width = comparison.width();
 
-            let doc=$(document).on('mousemove',(e)=> {
-              let offset = e.pageX - comparison.position().left;
-              let width = comparison.width();
+                // установим границы, чтобы ползунок не выходил
+                if (offset < 0) offset = 0;
+                if (offset > width) offset = width;
 
-              if (offset < 0) offset = 0;
-              if (offset > width) offset = width;
-
-              slider.css('left',offset + 'px');
-              before.css('width' , offset + 'px');
-
-
+                slider.css('left', offset + 'px');
+                before.css('width', offset + 'px');
             });
 
             doc.on('mouseup', () => doc.off('mousemove'));
         });
-}
+};
+
 imageComparison('#image-comparison');
