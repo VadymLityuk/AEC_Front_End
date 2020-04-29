37735e88551984cb120d3754f2a816844c7d8dc4
VadymLityuk <57510267+VadymLityuk@users.noreply.github.com>
2020-04-28 21:35:43 -0400

    netflixupdate

--- Programmation2/becflix/build/service-worker.js
+++ Programmation2/becflix/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e0fdd99e786e4b30ca7d6be5500b7b8.js"
+  "./precache-manifest.809d01b449f475c19c6c7e36436b759e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
