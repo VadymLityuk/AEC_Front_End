3faf6b9fd2775c5718c5eeb922a5449be25d7f98
VadymLityuk <57510267+VadymLityuk@users.noreply.github.com>
2020-08-20 21:25:55 -0400

    2

--- Integration1/Projet Final/service-worker.js
+++ Integration1/Projet Final/service-worker.js
@@ -1,12 +1,11 @@
 //Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v5';
+const CACHE_NAME = 'static-cache-v6';
 //Add list of files to cache here.
 const FILES_TO_CACHE = [
 
     'offline.html',
     'index.html',
     'explore,html',
-    'style.css',
     'About.html',
     'js/install.js',
 
