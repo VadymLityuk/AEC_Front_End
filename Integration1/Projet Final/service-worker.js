120935e0f7e79eb5c269e747fcbab02c2a2e5842
VadymLityuk <57510267+VadymLityuk@users.noreply.github.com>
2020-08-20 21:15:14 -0400

    4

--- Integration1/Projet Final/service-worker.js
+++ Integration1/Projet Final/service-worker.js
@@ -52,7 +52,7 @@ self.addEventListener('fetch', (evt) => {
             .catch(() => {
                 return caches.open(CACHE_NAME)
                     .then((cache) => {
-                        return cache.match('/AEC_Front_End/Integration1/Projet%20Final/offline.html' );
+                        return cache.match('/Projet Final/offline.html' );
                     });
             })
     );
