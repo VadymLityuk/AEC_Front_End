3d1bb1269126396b2d2578f55e3fe11124b2f755
VadymLityuk <57510267+VadymLityuk@users.noreply.github.com>
2020-08-25 19:32:23 -0400

    reactpwa

--- Programmation2/pwa/hellopwareact/build/service-worker.js
+++ Programmation2/pwa/hellopwareact/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2078ba2330f4a4f9db76d91ea202abe4.js"
+  "./precache-manifest.d1c6b2c9763a98caf4b3280b574eb28a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
