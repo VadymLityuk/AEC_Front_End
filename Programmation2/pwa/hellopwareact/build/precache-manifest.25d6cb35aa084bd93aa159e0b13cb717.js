715431125402d5f05cf47d8b5b066a4fb2b112a1
VadymLityuk <57510267+VadymLityuk@users.noreply.github.com>
2020-08-25 21:39:40 -0400

    reactpwa

---
+++ Programmation2/pwa/hellopwareact/build/precache-manifest.25d6cb35aa084bd93aa159e0b13cb717.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f30c966f387ae045f0a54973742ca8c2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6a287636b44dee070bad",
+    "url": "./static/css/main.a735ab53.chunk.css"
+  },
+  {
+    "revision": "3efc06ce748dfce751e3",
+    "url": "./static/js/2.9f058c5c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.9f058c5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a287636b44dee070bad",
+    "url": "./static/js/main.9586ec4d.chunk.js"
+  },
+  {
+    "revision": "622677ed61cd52f1c586",
+    "url": "./static/js/runtime-main.d559ae84.js"
+  },
+  {
+    "revision": "2a804c6b186125ef6999dcaf50ebbbc0",
+    "url": "./static/media/sp.2a804c6b.png"
+  }
+]);
