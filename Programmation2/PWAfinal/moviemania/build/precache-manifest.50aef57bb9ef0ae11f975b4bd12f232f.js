1958646e78b4a45cc6b39495ef8b5ec2bb35839c
VadymLityuk <57510267+VadymLityuk@users.noreply.github.com>
2020-09-15 22:23:43 -0400

    premiere

---
+++ Programmation2/PWAfinal/moviemania/build/precache-manifest.50aef57bb9ef0ae11f975b4bd12f232f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9540204c74b43d54da585b2febbcd7e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c18ac634095689faf944",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ffdb9aa41ba0cdd6bc5a",
+    "url": "./static/js/2.31a7e7bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.31a7e7bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c18ac634095689faf944",
+    "url": "./static/js/main.3a0317e8.chunk.js"
+  },
+  {
+    "revision": "4616820c7e0e521338ca",
+    "url": "./static/js/runtime-main.e0e8e70b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
