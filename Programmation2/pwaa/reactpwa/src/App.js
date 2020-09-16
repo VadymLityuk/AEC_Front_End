16ad5d3146cc69bf9a276d64ed2800b92ab9f7fe
VadymLityuk <57510267+VadymLityuk@users.noreply.github.com>
2020-09-16 00:14:56 -0400

    2

--- Programmation2/pwaa/reactpwa/src/App.js
+++ Programmation2/pwaa/reactpwa/src/App.js
@@ -11,7 +11,7 @@ import './components/accueil.scss'
 function App() {
   return (
 
-<React.Fragment>
+<>
     <Navi/>
     
       <Switch>
@@ -21,7 +21,7 @@ function App() {
      
       
     </Switch>
-    </React.Fragment>
+    </>
     
     
   );
