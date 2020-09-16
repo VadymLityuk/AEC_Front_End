2414323a571a9cb2510c492c02723fee0462a309
VadymLityuk <57510267+VadymLityuk@users.noreply.github.com>
2020-09-16 16:06:37 -0400

    185

---
+++ Integration2/Projet_Final/final/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {BrowserRouter as Router,Route,Switch } from 'react-router-dom';
+import  Accueil  from './components/Accueil';
+import Navi from './components/nav'
+import "bootstrap/dist/css/bootstrap.min.css";
+import './components/scss/nav.scss'
+import { Onlinecinema } from './components/Onlinecinema';
+import {TvShow} from './components/Films'
+
+import './App.scss';
+
+
+
+function App() {
+  return (
+  <React.Fragment>
+    
+    <Navi/>
+    <Router>
+    <Switch>
+      <Route path="/" exact  component={Accueil} />
+      <Route path="/Home" component={Accueil} />
+      <Route path="/Onlinecinema" component={Onlinecinema} />
+      <Route path="/Films" component={TvShow} />
+      
+    </Switch>
+    </Router>
+  </React.Fragment>
+  
+  );
+}
+
+export default App;
