535bbee89bd0a0b9b29804ccc7a66713c9c31403
VadymLityuk <57510267+VadymLityuk@users.noreply.github.com>
2020-10-06 20:40:58 -0400

    32

---
+++ Programmation3/my-app/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rot',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  body='';
+  
+  titrePrincipal = 'Cyberpunk ';
+  descript = 'Cyberpunk  is an open-world, action-adventure story set in Night City, a megalopolis obsessed with power, glamour and body modification. You play as V, a mercenary outlaw going after a one-of-a-kind implant that is the key to immortality.';
+ 
+  soustitre = 'get it now';
+  about ='about us';
+}
