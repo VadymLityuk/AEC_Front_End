b773832199ac9df73f62f03e7f9d8503dbc2d1cb
VadymLityuk <57510267+VadymLityuk@users.noreply.github.com>
2020-11-11 17:13:04 -0500

    imerialadd

--- Programmation3/app-voyages-VadymLityuk/src/app/formulaire/formulaire.component.ts
+++ Programmation3/app-voyages-VadymLityuk/src/app/formulaire/formulaire.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-formulaire',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulaire.component.css']
 })
 export class FormulaireComponent implements OnInit {
+  destinationsControl = new FormControl();
+  villesDepartControl = new FormControl();
+  villesDepart: string[] = ['Montréal','Québec', 'Kyiv'];
+  destinations: string[] = ['Panama','Cuba','Turquie' ];
 
   constructor() { }
 
--- Programmation3/app-voyages-VadymLityuk/src/app/mock-forfaits.ts
+++ Programmation3/app-voyages-VadymLityuk/src/app/mock-forfaits.ts
@@ -4,7 +4,7 @@ import { HOTELS } from "./mock-hotels";
 export const FORFAITS: Forfait[] = [
     {  id : "1",
         destination: "Panama City,Panama",
-        villeDeDepart: "Montreal",
+        villeDeDepart: "Montréal",
         hotel: HOTELS[0],
         dateDepart: "15 avril 2021",
         dateRetour: "21 avril 2021",
@@ -13,15 +13,25 @@ export const FORFAITS: Forfait[] = [
         forfaitVed: true,
      },
      {  id : "2",
-     destination: "Panama City,Panama",
-     villeDeDepart: "Montreal",
+     destination: "Cayo Santa Maria,Cuba",
+     villeDeDepart: "Montréal",
      hotel: HOTELS[1],
      dateDepart: "15 avril 2021",
      dateRetour: "21 avril 2021",
-     prix: 1890 ,
+     prix: 800 ,
      rabais : 70,
      forfaitVed: true,
-  }
+  },
+  {  id : "3",
+  destination: "Antalya,Turquie",
+  villeDeDepart: "Kyiv",
+  hotel: HOTELS[2],
+  dateDepart: "4 juin 2021",
+  dateRetour: "11 juin 2021",
+  prix: 1650 ,
+  rabais : 120,
+  forfaitVed: true,
+}
      
     
 ];
