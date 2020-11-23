bdd1272f324f88773078c4aeae70d391cbe14f88
VadymLityuk <57510267+VadymLityuk@users.noreply.github.com>
2020-11-23 18:15:19 -0500

    et

--- Programmation3/app-voyages-VadymLityuk/src/app/etoiles/etoiles.component.ts
+++ Programmation3/app-voyages-VadymLityuk/src/app/etoiles/etoiles.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit,Input } from '@angular/core';
 })
 export class EtoilesComponent implements OnInit {
 
-  @Input() nbetoiles: number;
+  @Input() nbetoiles: string;
 
   constructor() { }
 
