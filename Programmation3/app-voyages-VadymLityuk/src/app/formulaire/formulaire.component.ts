05c2d614ae41c36b32fcb5815624cd1132e54022
VadymLityuk <57510267+VadymLityuk@users.noreply.github.com>
2020-11-30 19:20:15 -0500

    filtre

--- Programmation3/app-voyages-VadymLityuk/src/app/formulaire/formulaire.component.ts
+++ Programmation3/app-voyages-VadymLityuk/src/app/formulaire/formulaire.component.ts
@@ -13,29 +13,46 @@ import { map, startWith } from 'rxjs/operators';
 
 export class FormulaireComponent implements OnInit {
 
-  destinationsControl = new FormControl();
-  villesDepartControl = new FormControl();
+  Control = new FormControl();
+  ControlDepart = new FormControl();
   filteredDestinations:  Observable<string[]>;
   filteredVillesDepart:  Observable<string[]>;
   villesDepart: string[] = ['Montréal','Québec', 'Kyiv'];
-  destinations: string[] ;
-
+  destinations: string[] =['Panama City','Antalya','Cayo Santa Maria'];
+  Filtredform: Observable<string[]>;
+  filtredDepart: Observable<string[]>;
   
 
   constructor() { }
+  
 
   ngOnInit(): void {
+    this.Filtredform = this.Control.valueChanges.pipe(
+      startWith(''),
+      map(value => this._myfilter(value))
+    );
+    this.filtredDepart = this.ControlDepart.valueChanges.pipe(
+    startWith(""),
+    map(value => this._myfilter2(value))
+  );
   }
+  
+  private _myfilter(value: string): string[] {
+    const filterValue = value.toLowerCase();
 
-  formatLabel(value: number) {
-    if (value >= 5) {
-      return Math.round(value / 5) + 'etoiles';
-    }
-
-    return value;
+    return this.villesDepart.filter(option => option.toLowerCase().includes(filterValue));
   }
+   private _myfilter2(value: string): string[] {
+    const filterValue2 = value.toLowerCase();
 
+    return this.destinations.filter(
+      destinations => destinations.toLowerCase().indexOf(filterValue2) === 0
+    );
+  }
+  
 }
+
+
 
 
 
