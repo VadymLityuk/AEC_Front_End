05db7a19971fea2beda3052dee6cb2fa60ef78ce
VadymLityuk <57510267+VadymLityuk@users.noreply.github.com>
2020-12-14 19:14:01 -0500

    navbar final adds

--- Programmation3/app-voyages-VadymLityuk/src/app/app-routing.module.ts
+++ Programmation3/app-voyages-VadymLityuk/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ForfaitCompComponent} from './forfait-comp/forfait-comp.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'Accueil', component: ForfaitCompComponent, pathMatch: 'full' },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Programmation3/app-voyages-VadymLityuk/src/app/forfait-comp/forfait-comp.component.ts
+++ Programmation3/app-voyages-VadymLityuk/src/app/forfait-comp/forfait-comp.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Forfait } from '../forfait';
+import { Hotel } from '../hotel';
+import { FORFAITS } from '../mock-forfaits';
+import {HOTELS} from '../mock-hotels'
+import { Formulaire } from '../formulaire';
 
 @Component({
   selector: 'app-forfait-comp',
@@ -6,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forfait-comp.component.css']
 })
 export class ForfaitCompComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  title = 'app-voyages-VadymLityuk';
+  mesForfaits: Array<Forfait> = FORFAITS;
+  mesHotels: Array<Hotel> = HOTELS;
+  formulaire :Formulaire = {
+    dateDepart: new Date,
+    duree: 0,
+    nbetoiles: 1,
+    caracterHotel: [''],
   }
 
+  ngOnInit(): void {
+    console.log('Mes forfaits',this.mesForfaits);
+    console.log('Mes Hotels',this.mesHotels);
+  }
 }
+
+
