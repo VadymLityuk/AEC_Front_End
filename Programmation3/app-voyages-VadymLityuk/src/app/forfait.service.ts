ff5cd226313328a22009aa06406fac2f12d0504b
VadymLityuk <57510267+VadymLityuk@users.noreply.github.com>
2021-01-14 20:15:01 -0500

    api

--- Programmation3/app-voyages-VadymLityuk/src/app/forfait.service.ts
+++ Programmation3/app-voyages-VadymLityuk/src/app/forfait.service.ts
@@ -11,27 +11,27 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class ForfaitService {
-    forfaitUrl = 'https://guarded-mesa-24051.herokuapp.com/api/';
+    forfaitUrl = 'https://forfaits-voyages.herokuapp.com/api/';
 
   constructor(private http: HttpClient) { }
 
   getForfait(): Observable<Forfait[]> {
-    return this.http.get<Forfait[]>(this.forfaitUrl + 'heros');  
+    return this.http.get<Forfait[]>(this.forfaitUrl + 'forfait');  
   }
 
   addForfait(forfait: Forfait): Observable<Forfait> {
-    return this.http.post<Forfait>(this.forfaitUrl + 'hero', forfait, httpOptions);
+    return this.http.post<Forfait>(this.forfaitUrl + 'forfait', forfait, httpOptions);
   }
 
       /** PUT: mise à jour du héros */
       updateForfait(forfait: Forfait): Observable<any> {
         const id = forfait._id;
-        return this.http.put<Forfait>(this.forfaitUrl + 'hero/' + id, forfait, httpOptions);
+        return this.http.put<Forfait>(this.forfaitUrl + 'forfait' + id, forfait, httpOptions);
     }
 
     /** DELETE: suppression du héros */
     deleteForfait(id: string): Observable<Forfait> {
-        return this.http.delete<Forfait>(this.forfaitUrl + 'hero/' + id, httpOptions);
+        return this.http.delete<Forfait>(this.forfaitUrl + 'forfait' + id, httpOptions);
     }
 
 
