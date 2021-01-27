b6e343573a13d43e635d57957b851e148e08da96
VadymLityuk <57510267+VadymLityuk@users.noreply.github.com>
2021-01-26 19:48:13 -0500

    styledpp

--- Vue/my-app/src/components/Header.vue
+++ Vue/my-app/src/components/Header.vue
@@ -3,9 +3,15 @@
       <router-link to="/"> home
                         <span class="far__glitch"></span>
                         <span class="far__label">home__</span></router-link> |
-      <router-link to="/contact">Contact</router-link> |
-      <router-link to="/inscription">Inscription</router-link> |
-      <router-link to="/login">Connexion</router-link>
+      <router-link to="/contact"> <span class="far__content" > Contact</span>
+                        <span class="far__glitch"></span>
+                        <span class="far__label">Contact__</span></router-link> |
+      <router-link to="/inscription">Inscription
+         <span class="far__glitch"></span>
+                        <span class="far__label">inscription_</span></router-link> |
+      <router-link to="/login">L0g In
+                        <span class="far__glitch"></span>
+                        <span class="far__label">L0g_ In__</span></router-link>
       </div>
   
 </template>
