15cf9cf8957fcfb0213822a0abd2bf02c79eb10c
VadymLityuk <57510267+VadymLityuk@users.noreply.github.com>
2021-02-02 19:10:08 -0500

    pass

--- Vue/my-app/src/views/Login.vue
+++ Vue/my-app/src/views/Login.vue
@@ -18,7 +18,7 @@
           </div>
           
           <div class="form-group">
-              <label for="motDePasse">Password</label>
+              <label for="pass">Password</label>
               <input v-model="motDePasse" required type="password" class="form-control" id="motDePasse" aria-describedby="passwordHelp">
               <small id="passwordHelp" class="form-text text-muted">!!</small>
           </div>
