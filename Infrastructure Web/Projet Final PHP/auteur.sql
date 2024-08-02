0085de63de25fb9ad5c339ab81b721b70e144897
Vadym Lityuk <57510267+VadymLityuk@users.noreply.github.com>
2024-08-02 09:59:39 -0400

    Update auteur.sql

--- Infrastructure Web/Projet Final PHP/auteur.sql
+++ Infrastructure Web/Projet Final PHP/auteur.sql
@@ -6,19 +6,6 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `auteur`
---
-
 CREATE TABLE `auteur` (
   `id` int(11) NOT NULL,
   `nom` varchar(25) COLLATE utf8mb4_general_ci NOT NULL,
@@ -27,9 +14,6 @@ CREATE TABLE `auteur` (
   `gender` varchar(25) COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Дамп данных таблицы `auteur`
---
 
 INSERT INTO `auteur` (`id`, `nom`, `prenom`, `pays`, `gender`) VALUES
 (1, 'Joanne', 'Rowling', 'Angleterre', 'f'),
@@ -38,23 +22,8 @@ INSERT INTO `auteur` (`id`, `nom`, `prenom`, `pays`, `gender`) VALUES
 (4, 'Clive', 'Lewis', 'Angleterre', 'm'),
 (5, 'John', 'Tolkien', 'Angleterre', 'm');
 
---
--- Индексы сохранённых таблиц
---
-
---
--- Индексы таблицы `auteur`
---
 ALTER TABLE `auteur`
   ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `auteur`
---
 ALTER TABLE `auteur`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
